8bbdb8580ee1b68ec03391cd9e5546ff7b20884a
ppetrov-dev <ppetrov@users.noreply.github.com>
2024-03-14 16:20:58 +0300

    add EEPROM

--- ArduinoNano/src/main.cpp
+++ ArduinoNano/src/main.cpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <Arduino.h>
+#include <EEPROM.h>
 #include "Configuration.h"
 
 #include <BroButton.h>
@@ -9,6 +10,7 @@ BroButton _sensorButtons[SECTION_COUNT];
 bool _relayStates[SECTION_COUNT];
 
 void ChangeRelayState(byte buttonPin);
+void RestoreRelayState(byte index);
 void setup()
 {
     Serial.begin(9600);
@@ -16,10 +18,12 @@ void setup()
         ;
     Serial.println("Serial OK");
 
-    for (auto i = 0; i < SECTION_COUNT; i++)
+    for (byte i = 0; i < SECTION_COUNT; i++)
     {
-        _relayStates[i] = RELAY_STATE_DEFAULT;
-        pinMode(RELAY_START_PIN + i, OUTPUT);
+        RestoreRelayState(i);
+        byte relayPin = RELAY_START_PIN + i;
+        pinMode(relayPin, OUTPUT);
+        digitalWrite(relayPin, _relayStates[i]);
 
         _sensorButtons[i].ChangePin(BUTTON_START_PIN + i);
         _sensorButtons[i].AttachOnClick(&ChangeRelayState);
@@ -29,13 +33,27 @@ void setup()
 
 void loop()
 {
-    for (auto i = 0; i < SECTION_COUNT; i++)
+    for (byte i = 0; i < SECTION_COUNT; i++)
         _sensorButtons[i].Tick();
 }
 
 byte GetIndex(byte buttonPin)
 {
     return buttonPin - BUTTON_START_PIN;
+}
+
+void RestoreRelayState(byte index)
+{
+    byte storedState = EEPROM.read(index);
+
+    if (storedState == 0xFF)// Проверка, было ли значение записано в EEPROM
+    {                                         
+        _relayStates[index] = RELAY_STATE_DEFAULT; // Установка значения по умолчанию, если EEPROM пустая
+        EEPROM.write(index, _relayStates[index]);      // Запись значения в EEPROM
+        return;
+    }
+
+    _relayStates[index] = storedState;
 }
 void ChangeRelayState(byte buttonPin)
 {
@@ -44,4 +62,7 @@ void ChangeRelayState(byte buttonPin)
     auto newState = !_relayStates[index];
     digitalWrite(RELAY_START_PIN + index, newState);
     _relayStates[index] = newState;
+
+    // Сохраняем новое состояние в EEPROM
+    EEPROM.write(index, newState);
 }
